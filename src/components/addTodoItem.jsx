717e3d1d3d405a0568d96153867242b47fcc4f47
Abhishek Patil <abishkpatil@gmail.com>
2024-07-12 22:06:41 +0530

    fix: converted it into form

--- src/components/addTodoItem.jsx
+++ src/components/addTodoItem.jsx
@@ -3,10 +3,11 @@ import { useState } from "react";
 
 const AddTodoItem = ({ setTodos }) => {
     const [task, setTask] = useState();
-    const handleAdd = async () => {
-        if(task.trim() === ""){
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if (task.trim() === "") {
             alert("Please enter the task");
-            return ;
+            return;
         }
         try {
             const res = await axios.post(import.meta.env.VITE_ENDPOINT + "/todos/add", {
@@ -18,10 +19,10 @@ const AddTodoItem = ({ setTodos }) => {
             console.log(error);
         }
     }
-    return <div style={{ display: 'flex', justifyContent: "center", gap: 10, marginTop: '5rem', alignItems: 'center' }}>
+    return <form onSubmit={handleSubmit} style={{ display: 'flex', justifyContent: "center", gap: 10, marginTop: '5rem', alignItems: 'center' }}>
         <input value={task} onChange={(e) => setTask(e.target.value)} type="text" placeholder="new task..." style={{ height: '2rem', minWidth: "20rem", fontSize: '18px', borderRadius: "4px", border: "1px solid gray", padding: "0rem 0.4rem" }} />
-        <button style={{ color: 'white', padding: "0.3rem 1.5rem" }} onClick={handleAdd}>Add</button>
-    </div>
+        <button style={{ color: 'white', padding: "0.3rem 1.5rem" }}>Add</button>
+    </form>
 }
 
 export default AddTodoItem;
