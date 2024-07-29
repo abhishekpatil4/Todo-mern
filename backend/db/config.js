800dadf41a004b0243ea998051061f6c2b8cccc8
Abhishek Patil <abishkpatil@gmail.com>
2024-07-29 23:21:49 +0530

    feat: add feat to connect to mongodb atlas

--- backend/db/config.js
+++ backend/db/config.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
+const dotenv = require("dotenv").config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb://127.0.0.1:27017/todos");
-        console.log("Connected to DB at mongodb://127.0.0.1:2707/todos");
+        await mongoose.connect(process.env.MONGODB_ATLAS_URI);
+        console.log("Connected to MongoDB Atlas");
     } catch (error) {
         console.log("Failed to connect to DB", error)
     }
