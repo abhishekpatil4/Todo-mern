2263a2d3f263c892bb26d190d0d2277401897169
Abhishek Patil <abishkpatil@gmail.com>
2024-07-30 10:33:48 +0530

    feat: add timestamps to schema

--- backend/db/schemas/todo.schema.js
+++ backend/db/schemas/todo.schema.js
@@ -2,6 +2,6 @@ const mongoose = require("mongoose");
 
 const todoSchema = new mongoose.Schema({
     task: { type: String, required: true },
-})
+}, { timestamps: true })
 
 module.exports = todoSchema;
